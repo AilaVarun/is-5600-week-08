c1c48e7167caec61aa0187772fd7281a17c00f1e
AilaVarun <156280955+AilaVarun@users.noreply.github.com>
2025-05-12 04:09:24 +0000

    Lab 8 Fullstack Prints Integration Testing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
+// module.exports = {
+//   // Uncomment this when you begin testing w/in-memory database
+//   // preset: '@shelf/jest-mongodb',
+//   testEnvironment: 'node',
+// };
+
 module.exports = {
   // Uncomment this when you begin testing w/in-memory database
-  // preset: '@shelf/jest-mongodb',
+  preset: '@shelf/jest-mongodb',
   testEnvironment: 'node',
 };
